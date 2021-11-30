25919bc6bf47acb2a02e61105c42350827dd51ec
dimelorobert <85129951+dimelorobert@users.noreply.github.com>
2021-11-30 11:31:26 +0100

    terminado faltan correcciones , divisa negativa, limitar decimales

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+     <div class="home">
+          <img alt="Vue logo" src="../assets/logo.png" />
+          <CurrencyBox
+               :config="config"
+               :culture="culture"
+               :amount="currencyAmount"
+          />
+     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import CurrencyBox from "@/components/CurrencyBox.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+     name: "Home",
+     components: {
+          HelloWorld,
+          CurrencyBox,
+     },
+     data() {
+          return {
+               // Currency Box Config parameters
+               config: {
+                    currency: "EUR",
+                    currencyDisplay: "symbol",
+                    currencySign: "standard",
+                    maximumFractionDigits: 2,
+                    minimumFractionDigits: 2,
+                    signDisplay: "auto",
+                    style: "currency",
+                    useGrouping: true,
+               },
+               currencyAmount: "",
+               culture: "es-ES",
+          };
+     },
+};
 </script>
