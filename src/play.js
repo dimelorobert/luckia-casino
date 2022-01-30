3ff5b5df1daf13cf71ab63ef489d1ab061e28725
codingwithrobert <airbusjayrobert@gmail.com>
2022-01-30 23:00:40 +0100

    playwright config

--- src/play.js
+++ src/play.js
@@ -8,40 +8,29 @@ const URL =
 	"https://resultados.isquad.es/equipo.php?seleccion=0&id_equipo=202137011&id=40989&id_territorial=33&id_ambito=3";
 
 (async () => {
-	const browser = await webkit.launch({ headless: false, slowMo: 50 });
+	const browser = await chromium.launch({ headless: true, slowMo: 50 });
 
 	const page = await browser.newPage();
 
 	await page.goto(URL);
 
-	const player = await page.$$eval(".caja_jugador", players => {
+	await page.waitForTimeout(6000);
+
+	const nameImgPlayer = await page.$$eval(".caja_jugador", players => {
 		const data = [];
 		players.forEach(jugador => {
 			const name = jugador.querySelector(".nombre_corto").innerText;
-			const img = jugador.querySelector(".foto_jugador>img").src;
-			data.push({ name, img });
+			const image = jugador.querySelector(".foto_jugador>img").src;
+			const feats = jugador.querySelectorAll(".item");
+			console.log("ESTOS SON LOS FEATS::>", feats);
+			data.push({ name, image  });
 		});
 		return data;
 	});
-
-	/*await page.type("input[id=cercadorOcultGoogle]", "Agenda cultural", {
-		delay: 250,
-	});
-
-	await page.type("input[id=cercadorOcultGoogle]", "Agenda cultural", {
-		delay: 75,
-	});
-
-	await page.click('input[type="submit"]');*/
-
-	//await page.close();
-	//await browser.close();
-	const plantilla = "a.pestanas[href='#plantilla']";
-	console.log(
-		"esta es plantilla:::::>",player
-	);
-
-	await page.click('a[href="#plantilla"]');
+	console.log("esta es plantilla:::::>", nameImgPlayer);
+	await page.close();
+	//await page.waitForTimeout(10000);
+	await browser.close();
 })().catch(err => {
 	console.log("[Error]::: ", err);
 	process.exit();
